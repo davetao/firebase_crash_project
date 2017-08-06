2aa85e4f9e3787b40fca62568a212ac564449ae5
David <david.t@b2cloud.com.au>
2017-08-07 09:28:57 +1000

    Added the firebase implementation

---
+++ app/src/main/java/com/davetao/firebasecrashreporting/TestApplication.kt
@@ -0,0 +1,25 @@
+package com.davetao.firebasecrashreporting
+
+import android.app.Application
+import android.os.StrictMode
+
+class TestApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build())
+
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build())
+        }
+    }
+}
