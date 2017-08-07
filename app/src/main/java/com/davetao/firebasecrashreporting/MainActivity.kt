8f574c505a8053cbb26ad28ea99afe19fb774b59
David <david.t@b2cloud.com.au>
2017-08-07 11:38:20 +1000

    Throwing a random crash

--- app/src/main/java/com/davetao/firebasecrashreporting/MainActivity.kt
+++ app/src/main/java/com/davetao/firebasecrashreporting/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            throw Exception("Random crash")
+        }
+        catch(e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
